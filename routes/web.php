6256a426e9644852a9b650edd8f9c4294f6fe721
Munerin22 <sg.7scy@gmail.com>
2021-01-20 22:54:53 +0900

    マルチログイン機能の追加

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,20 @@ Route::get('/detail/{id?}', 'ItemController@detail')->name('detail');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Userログイン
+Route::group(['middleware' => 'auth'], function() {;
+	Route::get('/home', 'HomeController@index')->name('home');
+});
+
+//Authログイン
+Route::group(['prefix' => 'admin', 'middleware' => 'guest:admin'], function() {
+	Route::get('/', function () {
+		return redirect('/admin/home');
+	});
+	Route::get('login', 'Admin\Auth\LoginController@showLoginForm')->name('admin.login');
+	Route::post('login', 'Admin\Auth\LoginController@login')->name('admin.login.submit');
+});
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
+	Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
+	Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+});
